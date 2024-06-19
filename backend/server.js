6b04b1c27d373c6baa0e6f5c36948945915227a8
nath-asha <omthasha@gmail.com>
2024-06-19 14:35:01 +0530

    tt

---
+++ backend/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  director: String,
+  genre: String,
+  year: Number
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+app.get('/movies', async (req, res) => {
+  const movies = await Movie.find();
+  res.json(movies);
+});
+
+app.post('/movies', async (req, res) => {
+  const newMovie = new Movie(req.body);
+  await newMovie.save();
+  res.json(newMovie);
+});
+
+app.put('/movies/:id', async (req, res) => {
+  const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedMovie);
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  await Movie.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Movie deleted' });
+});
+
+app.get('/search', async (req, res) => {
+  const { query } = req.query;
+  const movies = await Movie.find({
+    $or: [
+      { title: new RegExp(query, 'i') },
+      { director: new RegExp(query, 'i') },
+      { genre: new RegExp(query, 'i') }
+    ]
+  });
+  res.json(movies);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
